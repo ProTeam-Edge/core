d50f3bc5b005da9d5e712b5c532dc8384266a29d
Jonathan Manni <jmanni@proteamedge.com>
2020-07-28 21:59:46 +0000

    added files

---
+++ build_script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+apt update
+apt install -y openssh-client
+
+#mkdir ~/.ssh
+#touch ~/.ssh/known_hosts
+
+#ssh-keyscan -H somedomain.com >> ~/.ssh/known_hosts
+
+ssh -i my-ssh-key jmanni@34.105.28.87 'echo "hello world"'
