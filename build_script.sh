8e844c96548f760718c212006369ac9bdb9c0f7f
jondmanni <jondmanni@gmail.com>
2020-07-29 00:21:29 -0600

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -3,7 +3,7 @@ apt update
 apt install -y openssh-client
 
 mkdir ~/.ssh
-mv ./id_rsa ~/.ssh
+mv id_rsa ~/.ssh
 touch ~/.ssh/known_hosts
 
 ssh-keyscan 34.105.28.87 >> ~/.ssh/known_hosts
@@ -11,4 +11,4 @@ ssh-keyscan 34.105.28.87 >> ~/.ssh/known_hosts
 cat ~/.ssh/known_hosts
 echo "should've listed some things"
 
-ssh -o StrictHostKeyChecking=no -v jmanni@34.105.28.87 uptime 'bash -s cd_pull.bash'
+ssh -v jmanni@34.105.28.87 'bash -s cd_pull.bash'
