4f759ce799984b94f6c837a87ca566666d9ccfbe
jondmanni <jondmanni@gmail.com>
2020-07-29 12:22:38 -0600

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -2,10 +2,10 @@
 apt update
 apt install -y openssh-client
 
-mkdir ~/.ssh
-touch ~/.ssh/known_hosts
+#mkdir ~/.ssh
+#touch ~/.ssh/known_hosts
 
-ssh-keyscan 34.105.28.87 >> ~/.ssh/known_hosts
+#ssh-keyscan -H 34.105.28.87 >> ~/.ssh/known_hosts
 
 cat ~/.ssh/known_hosts
 echo "should've listed some things"
