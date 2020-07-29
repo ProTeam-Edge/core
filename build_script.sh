f0ad6dd9cc1da71a50ef8ebd3ace82f3573c69c9
jondmanni <jondmanni@gmail.com>
2020-07-29 11:38:18 -0600

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -11,4 +11,6 @@ ssh-keyscan 34.105.28.87 >> ~/.ssh/known_hosts
 cat ~/.ssh/known_hosts
 echo "should've listed some things"
 
-ssh -v jmanni@34.105.28.87 'bash -s cd_pull.bash'
+#ssh -v jmanni@34.105.28.87 'bash -s cd_pull.bash'
+#ssh -i unprotected_key -v jmanni@34.105.28.87 'cd /var/www/html/proteamedge/public/wp-content && git pull origin dev' 
+ssh -i unprotected_key -v jmanni@34.105.28.87 'bash -s cd_pull.bash' 
