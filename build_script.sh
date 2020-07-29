85bb8689139873906cf3bb8ba1f4511577d81a6e
jondmanni <jondmanni@gmail.com>
2020-07-29 11:49:22 -0600

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -12,4 +12,4 @@ echo "should've listed some things"
 
 #ssh -v jmanni@34.105.28.87 'bash -s cd_pull.bash'
 #ssh -i unprotected_key -v jmanni@34.105.28.87 'cd /var/www/html/proteamedge/public/wp-content && git pull origin dev' 
-ssh -i unprotected_key -T -v jmanni@34.105.28.87 'bash -s cd_pull.bash'
+ssh -i unprotected_key -V -t jmanni@34.105.28.87 'bash -s cd_pull.bash'
