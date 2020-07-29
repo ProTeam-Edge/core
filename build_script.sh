679fc372032ffdbae6158738e50816031c91cf02
Jonathan Manni <jmanni@proteamedge.com>
2020-07-29 03:37:16 +0000

    updated build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -2,9 +2,9 @@
 apt update
 apt install -y openssh-client
 
-#mkdir ~/.ssh
-#touch ~/.ssh/known_hosts
+mkdir ~/.ssh
+touch ~/.ssh/known_hosts
 
-#ssh-keyscan -H somedomain.com >> ~/.ssh/known_hosts
+ssh-keyscan -H 34.105.28.87 >> ~/.ssh/known_hosts
 
 ssh -i my-ssh-key jmanni@34.105.28.87 'echo "hello world"'
