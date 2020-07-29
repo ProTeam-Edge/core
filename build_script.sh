d33f4c6fa50a93d1e3411fc3a036a72cd184ac41
jondmanni <jondmanni@gmail.com>
2020-07-29 11:44:13 -0600

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -3,7 +3,6 @@ apt update
 apt install -y openssh-client
 
 mkdir ~/.ssh
-mv id_rsa ~/.ssh
 touch ~/.ssh/known_hosts
 
 ssh-keyscan 34.105.28.87 >> ~/.ssh/known_hosts
@@ -13,4 +12,4 @@ echo "should've listed some things"
 
 #ssh -v jmanni@34.105.28.87 'bash -s cd_pull.bash'
 #ssh -i unprotected_key -v jmanni@34.105.28.87 'cd /var/www/html/proteamedge/public/wp-content && git pull origin dev' 
-ssh -i unprotected_key -T -v jmanni@34.105.28.87 'bash -s cd_pull.bash' 
+ssh -i unprotected_key -T -v jmanni@34.105.28.87 'bash -s cd_pull.bash'
