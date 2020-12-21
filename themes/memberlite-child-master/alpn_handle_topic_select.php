e6736cef4935bbf05c0021c64c5d2352c9a5ee9f
Patrick Vermont <pvermont@proteamedge.com>
2020-12-21 12:58:17 -0800

    Back out

--- themes/memberlite-child-master/alpn_handle_topic_select.php
+++ themes/memberlite-child-master/alpn_handle_topic_select.php
@@ -4,8 +4,6 @@ include('/var/www/html/proteamedge/public/wp-blog-header.php');
 //TODO Check logged in, etc
 //TODO store HTML in MySql using htmlspecialchars()
 
-pp("IF YOU SEE THIS, PUSH TO ALCT.PRO WORKED");
-
 $replaceStrings = array();
 $html = $faxUx = $profileImageSelector = $topicLogoUrl = $emailUx = $proTeamHtml = $networkOptions = $topicOptions = $importantNetworkItems = $importantTopicItems = $interactionTypeSliders = $routes = $ownerFirst = $networkContactTopics = "";
 $qVars = $_POST;
