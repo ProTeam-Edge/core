5cd4d96356c09998b84f35aeb3fa3ef8b61b83bb
yashpro <eryash@proteamedge.com>
2020-12-24 10:01:29 +0530

    Added missing handle extension file
    Needed for chrome extension

---
+++ themes/memberlite-child-master/handle_extension.php
@@ -0,0 +1,29 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8" />
+		<title></title>
+
+	</head>
+
+	<body>
+
+		<div id = "root" >
+
+			PTE Test
+
+		</div>
+
+		<script src = "main.js" ></script>
+
+	</body>
+
+</html>
