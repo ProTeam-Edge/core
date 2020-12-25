73e8649dc7ed3920e714e71aca00b24c85155a84
Patrick Vermont <pvermont@proteamedge.com>
2020-12-24 16:16:05 -0800

    Test chrome extension changes

--- themes/memberlite-child-master/handle_extension.php
+++ themes/memberlite-child-master/handle_extension.php
@@ -3,27 +3,18 @@
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
-
 	<head>
-
 		<meta charset="utf-8" />
 		<title></title>
-
 	</head>
 
 	<body>
-
 		<div id = "root" >
-
-			PTE Test
-
+			<div class='pte_dialog_title'>ProTeam Edge File Uploader</div>
 		</div>
-
 		<script src = "main.js" ></script>
-
 	</body>
 
 </html>
