a99a6370721df9b257f1037a541a88a27c69889b
yashpro <eryash@proteamedge.com>
2021-01-01 16:56:01 +0530

    working on places for nonce
    working on places for nonce

--- themes/memberlite-child-master/alpn_client.js
+++ themes/memberlite-child-master/alpn_client.js
@@ -208,7 +208,7 @@ function pte_add_link_to_topic(data) {
 }
 
 function pte_get_form_for_link(tabId, dom_id) {
-
+	alert("reached places section");
 	if (dom_id) {
 		jQuery.ajax({
 			url: alpn_templatedir + 'pte_get_form_for_link.php',
