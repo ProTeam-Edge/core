c2f714a6a61b699fecc805d1ac21b4b944b929ac
yashpro <eryash@proteamedge.com>
2021-01-04 16:20:31 +0530

    updating nonce field
    updating nonce field

--- themes/memberlite-child-master/functions.php
+++ themes/memberlite-child-master/functions.php
@@ -40,7 +40,7 @@ function wpf_dev_process_before( $entry, $form_data ) {
 add_action( 'wpforms_process_before', 'wpf_dev_process_before', 10, 2 );
 function wpf_dev_display_submit_before( $form_data ) {
   
-   // return wp_nonce_field( 'wp_form_nonce', 'wp_form_nonce' );
+   return wp_nonce_field( 'wp_form_nonce', 'wp_form_nonce' );
 }
 add_action( 'wpforms_display_submit_before', 'wpf_dev_display_submit_before', 10, 1 );
 
