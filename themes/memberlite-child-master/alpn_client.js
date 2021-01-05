d0877f66dbc9b7ee4a0b28675e09cfae9efe8442
yashpro <eryash@proteamedge.com>
2021-01-05 17:21:15 +0530

    testing nonce for add template
    testing nonce for add template

--- themes/memberlite-child-master/alpn_client.js
+++ themes/memberlite-child-master/alpn_client.js
@@ -4761,13 +4761,12 @@ function pte_handle_template_operation(operation) {
 
 			allData.topic_type_key = topicTypeKey;
 			allData.topic_type_form_id = topicTypeFormId;
-			var security = specialObj.security;	
+
 			jQuery.ajax({
 				url: alpn_templatedir + 'alpn_handle_save_template.php',
 				type: 'POST',
 				data: {
-					template_data: JSON.stringify(allData),
-					security: security,
+					template_data: JSON.stringify(allData)
 				},
 				dataType: "json",
 				success: function(json) {
