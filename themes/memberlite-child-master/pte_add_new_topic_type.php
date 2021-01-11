bf6484e433fcced5a1434464a1a2ad0fc670bf3c
yashpro <eryash@proteamedge.com>
2021-01-11 13:45:32 +0530

    checking nonce via check_ajax_referer
    checking nonce via check_ajax_referer

--- themes/memberlite-child-master/pte_add_new_topic_type.php
+++ themes/memberlite-child-master/pte_add_new_topic_type.php
@@ -6,10 +6,15 @@ include('/var/www/html/proteamedge/public/wp-blog-header.php');
 
 $html="";
 $pVars = $_POST;
-$verify = 0;
-if(isset($pVars['security']) && !empty($pVars['security']))
-	$verify = wp_verify_nonce( $pVars['security'], 'alpn_script' );
-if($verify==1) {
+if(!is_user_logged_in() ) {
+	echo 'Not a valid request.';
+	die;
+}
+if(!check_ajax_referer('alpn_script', 'security',FALSE)) {
+   echo 'Not a valid request.';
+   die;
+}
+
 $sourceTopicTypeId = isset($pVars['source_topic_type_id']) ? $pVars['source_topic_type_id'] : 0;
 
 $userInfo = wp_get_current_user();
@@ -40,12 +45,6 @@ $returnResult = array(
 
 
 pte_json_out($returnResult);
-}
-else
-{
-	$html = 'Not a valid request.';
-	echo $html;
-	die;
-}
+
 
 ?>
