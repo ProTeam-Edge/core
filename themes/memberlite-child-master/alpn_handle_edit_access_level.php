c08bbeaf154c6860cec9bdbddd7e5e6ea6eeb78e
yashpro <eryash@proteamedge.com>
2021-01-11 16:58:49 +0530

    nonce via check_ajax_referer

--- themes/memberlite-child-master/alpn_handle_edit_access_level.php
+++ themes/memberlite-child-master/alpn_handle_edit_access_level.php
@@ -6,10 +6,15 @@ include('../../../wp-blog-header.php');
 $siteUrl = get_site_url();
 
 $qVars = $_GET;
-$verify = 0;
-if(isset($qVars['security']) && !empty($qVars['security']))
-	$verify = wp_verify_nonce( $pVars['security'], 'alpn_script' );
-if($verify==1) {
+if(!is_user_logged_in() ) {
+	echo 'Not a valid request.';
+	die;
+}
+if(!check_ajax_referer('alpn_script', 'security',FALSE)) {
+   echo 'Not a valid request.';
+   die;
+}
+
 $proTeamId = isset($qVars['proTeamId']) ? $qVars['proTeamId'] : '';
 $proTeamValue = isset($qVars['proTeamValue']) ? $qVars['proTeamValue'] : '';
 
@@ -23,10 +28,6 @@ if ($proTeamId && $userID) {
 }
 
 header('Content-Type: application/json');
-}
-else
-{
-	$results='Not a valid request.';
-}
+
 echo json_encode($results);
 ?>
