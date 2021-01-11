6abcec3a97f8ede88807e95e1c57dba3a5540343
yashpro <eryash@proteamedge.com>
2021-01-11 17:44:16 +0530

    nonce via check_ajax_referer

--- themes/memberlite-child-master/pte_get_topic_page.php
+++ themes/memberlite-child-master/pte_get_topic_page.php
@@ -2,16 +2,17 @@
 include('/var/www/html/proteamedge/public/wp-blog-header.php');
 
 $data = $_POST;
-$verify = 0;
-if(isset($data['security']) && !empty($data['security']))
-	$verify = wp_verify_nonce( $data['security'], 'alpn_script' );
-if($verify==1) {
+
+if(!is_user_logged_in() ) {
+	echo 'Not a valid request.';
+	die();
+}
+if(!check_ajax_referer('alpn_script', 'security',FALSE)) {
+   echo 'Not a valid request.';
+   die();
+}
+
 $pageNumber = pte_get_page_number($data);
 pte_json_out(array("page_number" => $pageNumber));
-}else
-{
-	$html = 'Not a valid request.';
-	echo $html;
-	die;
-}
+
 ?>
