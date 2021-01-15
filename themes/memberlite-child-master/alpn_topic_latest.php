3d24c0a4364558d986c1086df488ea4232b62143
yashpro <eryash@proteamedge.com>
2021-01-15 15:30:01 +0530

    nonce added

--- themes/memberlite-child-master/alpn_topic_latest.php
+++ themes/memberlite-child-master/alpn_topic_latest.php
@@ -3,6 +3,16 @@ include('../../../wp-blog-header.php');
 
 $userId = get_current_user_id();
 
+
+if(!is_user_logged_in() ) {
+	echo 'Not a valid request.';
+	die();
+}
+if(!check_ajax_referer('alpn_script', 'security',FALSE)) {
+   echo 'Not a valid request.';
+   die();
+}
+
 $results = $wpdb->get_results(
 	$wpdb->prepare("SELECT n.special, n.dom_id, n.last_op, n.topic_type_id, n.id, n.name, n.about, m.last_return_to FROM alpn_user_metadata m LEFT JOIN alpn_topics n ON m.last_topic_add_id = n.id WHERE m.id = %s", $userId)
 	);
