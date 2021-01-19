11f2c1794cf26c8bf718468af97d1319c0ad6c68
yashpro <eryash@proteamedge.com>
2021-01-19 17:52:35 +0530

    testing updating nonce for generateTopic.php
    testing updating nonce for generateTopic.php

--- themes/memberlite-child-master/jonathan_dev/generateTopic.php
+++ themes/memberlite-child-master/jonathan_dev/generateTopic.php
@@ -1,6 +1,19 @@
 <?php
   // Get data from POST
+include('/var/www/html/proteamedge/public/wp-blog-header.php');
 if(!is_user_logged_in() ) {
+	echo 'Not a valid request.';
+	die;
+}
+$passed = 0;
+$nonce  = $_POST["security"];
+$verify = wp_verify_nonce($nonce, 'form-generate' );
+if($verify==1) 
+{
+	$passed = 1;
+}
+if($passed==0)
+{
 	echo 'Not a valid request.';
 	die;
 }
