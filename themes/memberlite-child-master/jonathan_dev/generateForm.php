48a5ae2620998b545b9e63dbd2b40be80a1879c4
yashpro <eryash@proteamedge.com>
2021-01-19 16:11:13 +0530

    updating nonce for generateForm.php
    updating nonce for generateForm.php

--- themes/memberlite-child-master/jonathan_dev/generateForm.php
+++ themes/memberlite-child-master/jonathan_dev/generateForm.php
@@ -1,6 +1,7 @@
 <?php
 include('/var/www/html/proteamedge/public/wp-blog-header.php');
 $passed = 0;
+$nonce  = $_POST["security"];
 $verify = wp_verify_nonce($nonce, 'security' );
 if($verify==1) 
 {
