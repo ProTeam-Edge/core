95e137fbe00d4d7aaccaa310de51182d4055b4cc
yashpro <eryash@proteamedge.com>
2021-01-25 10:50:07 +0530

    Optimizing manage topic types section
    Optimizing manage topic types section

--- plugins/proteamedge/proteamedge.php
+++ plugins/proteamedge/proteamedge.php
@@ -467,7 +467,8 @@ $nonce = wp_create_nonce( 'admin_test');
       }
 
       function addRows ( row , output) {
-	
+		 $.LoadingOverlay("show");
+		 return false;
 			console.log(output);
 			console.log('consoled output')
 		
