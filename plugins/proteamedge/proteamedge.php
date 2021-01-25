6d7295276c923c923252132b1b2b2cf79208ce41
yashpro <eryash@proteamedge.com>
2021-01-25 10:27:59 +0530

    Optimizing manage topic types section
    Optimizing manage topic types section

--- plugins/proteamedge/proteamedge.php
+++ plugins/proteamedge/proteamedge.php
@@ -596,7 +596,7 @@ $nonce = wp_create_nonce( 'admin_test');
       }
 
       function format ( row ) {
-		  alert("reached click")
+		 $.LoadingOverlay("show");
         var d = row.data();
         // Make ajax request to php to get properties for this class
         var output;
