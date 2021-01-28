e273c4844179d5f35ded7662965f54b10c26eab0
yashpro <eryash@proteamedge.com>
2021-01-28 15:53:25 +0530

    Optimizing manage topic types section
    Optimizing manage topic types section

--- themes/memberlite-child-master/topics/update_manage_topic_settings.php
+++ themes/memberlite-child-master/topics/update_manage_topic_settings.php
@@ -1,5 +1,6 @@
 <?php
 include('/var/www/html/proteamedge/public/wp-blog-header.php');
+global $wpdb;
 if(!is_user_logged_in() ) {
 	echo 'Not a valid request.';
 	die;
@@ -7,16 +8,31 @@ if(!is_user_logged_in() ) {
 $passed = 0;
 $nonce  = $_POST["security"];
 $verify = wp_verify_nonce($nonce, 'admin_test' );
-if($verify==1) 
-{
+if($verify==1) {
 	$passed = 1;
 }
-if($passed==0)
-{
+if($passed==0) {
 	echo 'Not a valid request.';
 	die;
 }
- echo '<pre>';
- print_r($_POST);
- DIE;
+$field_type = $_POST['field_type'];
+$topic_name = $_POST['topic_name'];
+$value = $_POST['value'];
+$sql = 'select * from alpn_manage_topic where topic_name= "'.$topic_name.'"';
+$data = $wpdb->get_row($sql);
+$core_topic = 0;
+$hide_properties = 0;
+$friendly_name = '';
+$visibility = '';
+if($field_type=='linked_topic') {
+	$core_topic = $value 
+}
+if(empty($data)) {
+	$sql = 'insert into alpn_manage_topic(topic_name,core_topic,hide_properties,friendly_name,visibility,cdate,mdate)values("'.$topic_name.'","'.$core_topic.'","'.$hide_properties.'","'.$friendly_name.'","'.$visibility.'","'.time().'","'.time().'")';
+	
+}
+else {
+	$sql = 'update alpn_manage_topic set topic_name = "'.$topic_name.'",core_topic="'.$core_topic.'",hide_properties="'.$hide_properties.'",friendly_name="'.$friendly_name.'",visibility="'.$visibility.'",mdate="'.time().'" where topic_name="'.$topic_name.'"';
+}
+$data = $wpdb->query($sql);
 ?>
