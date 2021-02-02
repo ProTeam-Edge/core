160a0e9537addbbd585c0333af3380647d812d3a
yashpro <eryash@proteamedge.com>
2021-02-02 12:10:51 +0530

    Optimizing manage topic types section
    Optimizing manage topic types section

--- plugins/proteamedge/proteamedge.php
+++ plugins/proteamedge/proteamedge.php
@@ -430,6 +430,7 @@ $nonce = wp_create_nonce( 'admin_test');
 			});
 		var primary_types = ['Time','Text','DateTime','Number','Date','Boolean','URL'];
 		merged =  $.merge( checked_ids, primary_types );
+		console.log('merged')
 		console.log(merged)
         var output="";
         var subPropertyOutput;
