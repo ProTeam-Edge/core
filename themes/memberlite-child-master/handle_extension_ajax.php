0db09872d69c17b4d4062e9c5a30c89e65a6eb6f
yashpro <eryash@proteamedge.com>
2021-02-08 16:53:09 +0530

    extension endpoint
    extension endpoint

--- themes/memberlite-child-master/handle_extension_ajax.php
+++ themes/memberlite-child-master/handle_extension_ajax.php
@@ -1,21 +1,4 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'].'/wp-blog-header.php');
-if(!is_user_logged_in() ) {
-	echo 'Not a valid request.';
-	die;
-}
-/* $passed = 0;
-$nonce  = $_POST["security"];
-$verify = wp_verify_nonce($nonce, 'admin_test' );
-if($verify==1) 
-{
-	$passed = 1;
-}
-if($passed==0)
-{
-	echo 'Not a valid request.';
-	die;
-} */
 
 echo '<pre>';
 print_r($_POST);
