0ade482a4f8692d812ca0c712ec42bbcda593759
yashpro <eryash@proteamedge.com>
2021-02-08 17:13:01 +0530

    extension endpoint
    extension endpoint

--- themes/memberlite-child-master/handle_extension_ajax.php
+++ themes/memberlite-child-master/handle_extension_ajax.php
@@ -1,6 +1,9 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
-
+if(!is_user_logged_in() ) {
+	echo 'Not a valid request.';
+	die;
+}
 echo '<pre>';
 echo '<h3>Converted blob to form file instance now ready to be uploaded</h3>';
 print_r($_POST);
