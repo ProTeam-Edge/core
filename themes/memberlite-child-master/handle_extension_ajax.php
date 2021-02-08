4be0c6fb7cdf31c124e9ef68eb17ad607830e269
yashpro <eryash@proteamedge.com>
2021-02-08 17:45:47 +0530

    extension endpoint
    extension endpoint

--- themes/memberlite-child-master/handle_extension_ajax.php
+++ themes/memberlite-child-master/handle_extension_ajax.php
@@ -1,6 +1,7 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
 $root = $_SERVER['DOCUMENT_ROOT'];
+$site_url = site_url();
 $child_theme_path = $root.'/wp-content/themes/memberlite-child-master/attachments/'; 
 $passed = 0;
 $nonce  = $_POST["security"];
@@ -21,7 +22,8 @@ print_r($_POST);
 print_r($_FILES);
 
 if (move_uploaded_file($_FILES['file']['tmp_name'], $child_theme_path.$name)) {
-    echo "File Uploaded Successfully";
+    echo "File Uploaded Successfully<br/>";
+    echo "Path : ".$site_url."/wp-content/themes/memberlite-child-master/attachments/".$name."";
 }
 else
 	echo "Not uploaded";
