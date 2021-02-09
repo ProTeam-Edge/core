68a246aaded3d148b136dd957621d19217cc691d
yashpro <eryash@proteamedge.com>
2021-02-09 16:46:25 +0530

    Optimizing manage topic types section
    Optimizing manage topic types section

--- themes/memberlite-child-master/topics/generateTopic.php
+++ themes/memberlite-child-master/topics/generateTopic.php
@@ -25,10 +25,9 @@ if($passed==0)
 
   //$topicName = gettype($post_data);
 $topicName  =  $post_data->topic_name;
-$alpn_about_sql = 'select alpn_about_source from alpn_manage_topic where topic_name = "'.$topicName.'"';
-$alpn_about_data = $wpdb->get_row($alpn_about_sql);
-$alpn_name_sql = 'select alpn_name_source from alpn_manage_topic where topic_name = "'.$topicName.'"';
-$alpn_name_data = $wpdb->get_row($alpn_name_sql);
+$alpn_sql = 'select alpn_about_source,alpn_name_source from alpn_manage_topic where topic_name = "'.$topicName.'"';
+$alpn_data = $wpdb->get_row($alpn_sql);
+
 
   // Make sure POST data is stored as an array; typecast it as such
   $post = (array) $post_data;
@@ -49,51 +48,47 @@ foreach($decode_db_string as $keys=>$vals)
 }
 
 /* 	$addition_array['pte_added_date']['id']=$last_id+1;
-$addition_array['pte_added_date']['friendly']="Added Date";
-$addition_array['pte_added_date']['type']="Date";
-$addition_array['pte_added_date']['name']="pte_added_date";
-$addition_array['pte_added_date']['required']="false";
-$addition_array['pte_added_date']['schema_key']="pte_added_Date";
-$addition_array['pte_added_date']['hidden']="false";
-$addition_array['pte_added_date']['hidden_print']="true";
+	$addition_array['pte_added_date']['friendly']="Added Date";
+	$addition_array['pte_added_date']['type']="Date";
+	$addition_array['pte_added_date']['name']="pte_added_date";
+	$addition_array['pte_added_date']['required']="false";
+	$addition_array['pte_added_date']['schema_key']="pte_added_Date";
+	$addition_array['pte_added_date']['hidden']="false";
+	$addition_array['pte_added_date']['hidden_print']="true";
 
-$addition_array['pte_modified_date']['id']=$last_id+2;
-$addition_array['pte_modified_date']['friendly']="Modified Date";
-$addition_array['pte_modified_date']['type']="Date";
-$addition_array['pte_modified_date']['name']="pte_modified_date";
-$addition_array['pte_modified_date']['required']="false";
-$addition_array['pte_modified_date']['schema_key']="pte_modified_Date";
-$addition_array['pte_modified_date']['hidden']="false";
-$addition_array['pte_modified_date']['hidden_print']="true";
+	$addition_array['pte_modified_date']['id']=$last_id+2;
+	$addition_array['pte_modified_date']['friendly']="Modified Date";
+	$addition_array['pte_modified_date']['type']="Date";
+	$addition_array['pte_modified_date']['name']="pte_modified_date";
+	$addition_array['pte_modified_date']['required']="false";
+	$addition_array['pte_modified_date']['schema_key']="pte_modified_Date";
+	$addition_array['pte_modified_date']['hidden']="false";
+	$addition_array['pte_modified_date']['hidden_print']="true";
 
-$addition_array['pte_image_logo']['id']=$last_id+3;
-$addition_array['pte_image_logo']['friendly']="Image/Logo";
-$addition_array['pte_image_logo']['type']="image";
-$addition_array['pte_image_logo']['name']="pte_image_logo";
-$addition_array['pte_image_logo']['required']="false";
-$addition_array['pte_image_logo']['schema_key']="pte_image_URL";
-$addition_array['pte_image_logo']['hidden']="false";
-$addition_array['pte_image_logo']['hidden_print']="true"; */
+	$addition_array['pte_image_logo']['id']=$last_id+3;
+	$addition_array['pte_image_logo']['friendly']="Image/Logo";
+	$addition_array['pte_image_logo']['type']="image";
+	$addition_array['pte_image_logo']['name']="pte_image_logo";
+	$addition_array['pte_image_logo']['required']="false";
+	$addition_array['pte_image_logo']['schema_key']="pte_image_URL";
+	$addition_array['pte_image_logo']['hidden']="false";
+	$addition_array['pte_image_logo']['hidden_print']="true"; */
 	$obj = json_decode (json_encode ($addition_array), FALSE);
-
 	$obj_merged = (object) array_merge((array) $field_map, (array) $obj);
-
- $final_array = array();
- if(!empty($alpn_about_data))
+	$final_array = array();
+if(!empty($alpn_data->alpn_about_source))
 {
-	$about_source = $alpn_about_data->alpn_about_source;
-echo '<pre>';
-print_r($about_source);
-  $alpn_about_source_obj = json_decode($about_source,true);
-echo 'next';
-print_r($alpn_about_source_obj);
-die;
-  $final_array['alpn_about_source'] = $alpn_about_source_obj->alpn_about_source;
+	$about_source = $alpn_data->alpn_about_source;
+	$alpn_about_source_obj = json_decode($about_source,true);
+	$alpn_about_source_obj_final = json_decode (json_encode ($alpn_about_source_obj->alpn_about_source), FALSE);
+	$final_array['alpn_about_source'] = $alpn_about_source_obj_final;
 }
-if(!empty($alpn_name_data))
+if(!empty($alpn_data->alpn_name_source))
 {
-	$alpn_name_data_obj = json_decode ($alpn_name_data);	
-  $final_array['alpn_name_source'] = $alpn_name_data_obj->alpn_name_source;
+	$name_source = $alpn_data->alpn_name_source;
+	$alpn_name_source_obj = json_decode($name_source,true);
+	$alpn_name_source_obj_final = json_decode (json_encode ($alpn_name_source_obj->alpn_about_source), FALSE);
+	$final_array['alpn_name_source'] = $alpn_name_source_obj_final;
 }
 foreach($post as $keys=>$vals)
 {
