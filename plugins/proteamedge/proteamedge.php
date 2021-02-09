c96693c439f10ba3deb027244153d75979ac4051
yashpro <eryash@proteamedge.com>
2021-02-09 11:26:49 +0530

    Optimizing manage topic types section
    Optimizing manage topic types section

--- plugins/proteamedge/proteamedge.php
+++ plugins/proteamedge/proteamedge.php
@@ -3479,11 +3479,13 @@ $site_url = site_url();
 			<br/>
             <textarea id='save_extra_fields' class='pte_topic_part_textarea'>".$get_option."</textarea>
 			<a onclick='return save_extra_fields()' style='color: rgb(0, 116, 187);cursor: pointer;text-decoration:none;margin:5px 0 0 10px;' href='javascript:void(0)' class='update_extra_fields'>Update</a>
-			<h3>alpn_about_source JSON</h3>
 			<br/>
+			<br/>
+			<h3>alpn_about_source JSON</h3>
 			<br/>
             <textarea id='alpn_about_source_textarea' class='pte_topic_part_textarea'></textarea>
 			<a onclick='return save_alpn(this)' style='color: rgb(0, 116, 187);cursor: pointer;text-decoration:none;margin:5px 0 0 10px;' href='javascript:void(0)'>Update</a>
+			<br/>
 			<h3>alpn_name_source JSON</h3>
 			<br/>
 			<br/>
