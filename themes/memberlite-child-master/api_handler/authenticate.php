9e2b072e1d471b0623b3348e430ebd43796f8113
yashpro <eryash@proteamedge.com>
2021-02-17 17:25:15 +0530

    API handler
    API handler

--- themes/memberlite-child-master/api_handler/authenticate.php
+++ themes/memberlite-child-master/api_handler/authenticate.php
@@ -9,7 +9,7 @@ $data = json_decode($input);
 
 //Wordpress authentication
 global $wpdb;
-$data = array();
+$response_data = array();
 $email = $data->email;
 $password = $data->password_field;
 if(!empty($email) && !empty($password))
@@ -21,12 +21,12 @@ if(!empty($email) && !empty($password))
 	else {
 		if ( $user && wp_check_password( $password, $verify->data->user_pass, $verify->ID ) ) {
 			$hash = md5('proteamedge'.$verify->data->user_pass.$verify->ID.time());
-			$data['ID'] = $verify->ID;
-			$data['username'] = $verify->data->username;
-			$data['email'] = $verify->data->email;
-			$data['token'] = $hash;
+			$response_data['ID'] = $verify->ID;
+			$response_data['username'] = $verify->data->username;
+			$response_data['email'] = $verify->data->email;
+			$response_data['token'] = $hash;
 			$update_option = update_option('api_request_token_'.$verify->ID.'',$hash);
-			$response = array('success' => 1, 'message'=>'User found successfully.','token'=>$data);
+			$response = array('success' => 1, 'message'=>'User found successfully.','data'=>$response_data);
 		} else {
 			$response = array('success' => 0, 'message'=>'Please input correct password and try again');
 		}
