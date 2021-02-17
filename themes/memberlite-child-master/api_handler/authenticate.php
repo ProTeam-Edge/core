e990f38bf19dea2f59670e84818cb4671f3b69c5
yashpro <eryash@proteamedge.com>
2021-02-17 17:05:20 +0530

    API handler
    API handler

--- themes/memberlite-child-master/api_handler/authenticate.php
+++ themes/memberlite-child-master/api_handler/authenticate.php
@@ -1,10 +1,17 @@
 <?php 
+include('/var/www/html/proteamedge/public/wp-blog-header.php');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
+
+//Fetching request
 $input = file_get_contents('php://input');
 $data = json_decode($input);
-$action = $data->action;
-function Authenticate($data) {
-}
-$action
+
+//Wordpress authentication
+global $wpdb;
+$email = $data->email;
+$password = $data->password_field;
+$verify = get_user_by('email',$email );
+echo '<pre>';
+print_r($verify);
 ?>
