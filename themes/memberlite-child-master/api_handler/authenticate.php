e8c2b056d8c42e45f97a363fc7198f3dcc002dec
yashpro <eryash@proteamedge.com>
2021-02-17 17:01:01 +0530

    API handler
    API handler

--- themes/memberlite-child-master/api_handler/authenticate.php
+++ themes/memberlite-child-master/api_handler/authenticate.php
@@ -3,5 +3,8 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
 $input = file_get_contents('php://input');
 $data = json_decode($input);
-print_r($data);
+$action = $data->action;
+function Authenticate($data) {
+}
+$action
 ?>
