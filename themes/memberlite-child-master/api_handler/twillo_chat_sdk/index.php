e8e02383e9f5c5adbd15c77fd08a97444a782306
yashpro <eryash@proteamedge.com>
2021-02-22 12:01:28 +0530

    Twillo Chat Sdk
    Twillo Chat Sdk

---
+++ themes/memberlite-child-master/api_handler/twillo_chat_sdk/index.php
@@ -0,0 +1,43 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
+echo '<pre>';
+print_r($_SERVER);
+DIE;
+
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+$username = $data->username;
+// Get the PHP helper library from https://twilio.com/docs/libraries/php
+require_once 'E:/Development/wamp64/www/twillo/vendor/autoload.php'; // Loads the library
+use Twilio\Jwt\AccessToken;
+use Twilio\Jwt\Grants\ChatGrant;
+
+// Required for all Twilio access tokens
+$twilioAccountSid = 'AC9ff1cc4c98a58c86d4e1942034ec6d41';
+$twilioApiKey = 'SK4f18b268558283cb58ed64093a083acc';
+$twilioApiSecret = 'nrDvX76GLS39xq3JPxovT2gXPHxNnkYr';
+
+// Required for Chat grant
+$serviceSid = 'IS0e86f36674e9492c8aede1549c216784';
+// choose a random username for the connecting user
+$identity =$username;
+
+// Create access token, which we will serialize and send to the client
+$token = new AccessToken(
+    $twilioAccountSid,
+    $twilioApiKey,
+    $twilioApiSecret,
+    3600,
+    $identity
+);
+
+// Create Chat grant
+$chatGrant = new ChatGrant();
+$chatGrant->setServiceSid($serviceSid);
+
+// Add grant to token
+$token->addGrant($chatGrant);
+
+// render token to string
+echo $token->toJWT();
