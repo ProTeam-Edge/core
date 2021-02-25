2b4358be9f6ad6b91a856acfef1fe60f9974c4c4
yashpro <eryash@proteamedge.com>
2021-02-25 12:35:41 +0530

    Twillo Chat Sdk
    Twillo Chat Sdk

--- themes/memberlite-child-master/api_handler/get_chat_token.php
+++ themes/memberlite-child-master/api_handler/get_chat_token.php
@@ -3,7 +3,7 @@ include_once('../../pte_config.php');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
 $root = $_SERVER['DOCUMENT_ROOT'];
-require_once $root.'/wp-content/themes/memberlite-child-master/api_handler/twillo_chat_sdk/vendor/autoload.php';
+require_once $root.'/wp-content/themes/memberlite-child-master/api_handler/sdk/vendor/autoload.php';
 use Twilio\Jwt\AccessToken;
 use Twilio\Jwt\Grants\ChatGrant;
 
