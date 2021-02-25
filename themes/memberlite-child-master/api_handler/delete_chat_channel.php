a76b5473690a373c719072936896b8b1c906e3b8
yashpro <eryash@proteamedge.com>
2021-02-25 13:05:55 +0530

    Twillo Chat Sdk

--- themes/memberlite-child-master/api_handler/delete_chat_channel.php
+++ themes/memberlite-child-master/api_handler/delete_chat_channel.php
@@ -5,7 +5,6 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once $root.'/wp-content/themes/memberlite-child-master/api_handler/sdk/vendor/autoload.php';
 use Twilio\Rest\Client;
-
 $input = file_get_contents('php://input');
 $data = json_decode($input);
 if(isset( $data->id) && !empty($data->id)) {
@@ -14,7 +13,7 @@ if(isset( $data->id) && !empty($data->id)) {
 	$token =AUTHTOKEN;
 	$cservice =CHATSERVICESID;
 	$twilio = new Client($sid, $token);
-	$call = $twilio->chat->v2->services($cservice)->channels("CHc12cfcabc00f4d3c8fb4cab900868de7")->delete();
+	$call = $twilio->chat->v2->services($cservice)->channels($id)->delete();
 	if($call==1) {
 		$response = array('success' => 1, 'message'=>'Channel deleted.');
 	}
