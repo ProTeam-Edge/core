2a2081e2a701849bed22fa8de7b94dc98e6472cb
yashpro <eryash@proteamedge.com>
2021-02-25 11:34:17 +0530

    Twillo Chat Sdk
    Twillo Chat Sdk

---
+++ themes/memberlite-child-master/api_handler/twillo_chat_sdk/delete_chat_channel.php
@@ -0,0 +1,17 @@
+<?php 
+include_once('../../pte_config.php');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
+$root = $_SERVER['DOCUMENT_ROOT'];
+require_once $root.'/wp-content/themes/memberlite-child-master/api_handler/twillo_chat_sdk/vendor/autoload.php';
+use Twilio\Rest\Client;
+$sid = ACCOUNT_SID;
+$token =APIKEY;
+$twilio = new Client($sid, $token);
+
+$response = $twilio->chat->v2->services(CHATSERVICESID)
+                 ->channels("CHc12cfcabc00f4d3c8fb4cab900868de7")
+                 ->delete();
+				 echo '<pre>';
+				 print_r($response);
+?>
