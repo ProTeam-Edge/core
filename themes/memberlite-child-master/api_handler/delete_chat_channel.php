fd0e5bc283aa00efc7cf97c9bc7d2c083ad2d161
yashpro <eryash@proteamedge.com>
2021-02-25 12:58:24 +0530

    Twillo Chat Sdk
    Twillo Chat Sdk

--- themes/memberlite-child-master/api_handler/delete_chat_channel.php
+++ themes/memberlite-child-master/api_handler/delete_chat_channel.php
@@ -5,14 +5,22 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once $root.'/wp-content/themes/memberlite-child-master/api_handler/sdk/vendor/autoload.php';
 use Twilio\Rest\Client;
-$sid = ACCOUNT_SID;
-$token =AUTHTOKEN;
-$cservice =CHATSERVICESID;
-$twilio = new Client($sid, $token);
 
-$response = $twilio->chat->v2->services($cservice)
-                 ->channels("CHc12cfcabc00f4d3c8fb4cab900868de7")
-                 ->delete();
-				 echo '<pre>';
-				 print_r($response);
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+if(isset( $data->id) && !empty($data->id)) {
+	$id = $data->id;
+	$sid = ACCOUNT_SID;
+	$token =AUTHTOKEN;
+	$cservice =CHATSERVICESID;
+	$twilio = new Client($sid, $token);
+	$call = $twilio->chat->v2->services($cservice)->channels("CHc12cfcabc00f4d3c8fb4cab900868de7")->delete();
+	if($call==1) {
+		$response = array('success' => 1, 'message'=>'Channel deleted.');
+	}
+}
+else {
+	$response = array('success' => 0, 'message'=>'No valid channel id passed.');
+}
+echo json_encode($response);
 ?>
