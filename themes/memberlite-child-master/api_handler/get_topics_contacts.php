961ed35be1509843bfa7a045457ce0d3ad2fc00a
yashpro <eryash@proteamedge.com>
2021-02-26 17:19:26 +0530

    Chat Api
    Chat Api

--- themes/memberlite-child-master/api_handler/get_topics_contacts.php
+++ themes/memberlite-child-master/api_handler/get_topics_contacts.php
@@ -10,10 +10,10 @@ $data = json_decode($input);
 //$id = $data->id;
 $id = $_REQUEST['id'];
 $sql = 'select * from alpn_topics where owner_id = "'.$id.'" and special = "contact"';
-$data = $wpdb->get_results($sql);
+$result = $wpdb->get_results($sql);
 $array = array();
-if(!empty($data) {
-	foreach($data as $val) {
+if(!empty($result) {
+	foreach($result as $val) {
 		$array[$val->id]['name'] = $val->name];
 	}
 	$response = array('success' => 1, 'message'=>'Contacts found.','data'=>$array);
