1f364fcb34520d3ec7687ee6c3a91d8d1625e95e
yashpro <eryash@proteamedge.com>
2021-03-03 17:16:37 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_personal_information.php
+++ themes/memberlite-child-master/api_handler/get_personal_information.php
@@ -11,17 +11,25 @@ $id = $data->id;
 $sql = "SELECT * from alpn_topics where owner_id = ".$id." and special = 'user' and sync_id !=''";
 $results = $wpdb->get_row($sql);
 $topic_content = '';
+$array = array();
 if(isset($results->topic_content) && !empty($results->topic_content))
 {
-	echo '<pre>';
-	print_r(json_decode($results->topic_content));
-	die;
-$topic_content_response = json_decode($results->topic_content);
-$topic_content = json_encode($topic_content_response, JSON_UNESCAPED_SLASHES);
-
+	$topic_content_response = json_decode($results->topic_content);
+	if(!empty($topic_content_response))
+	{
+		$array['person_givenname'] =$topic_content_response->person_givenname;
+		$array['person_familyname'] =$topic_content_response->person_familyname;
+		$array['person_jobtitle'] =$topic_content_response->person_jobtitle;
+		$array['person_email'] =$topic_content_response->person_email;
+		$array['person_url'] =$topic_content_response->person_url;
+		$array['person_telephone'] =$topic_content_response->person_telephone;
+		$array['person_faxnumber'] =$topic_content_response->person_faxnumber;
+		$array['person_knowsabout'] =$topic_content_response->person_knowsabout;
+		$array['person_description'] =$topic_content_response->person_description;
+	}
 }
-if(!empty($topic_content))
-$response = array('success' => 1, 'message'=>'Success topics found.','data'=>$topic_content);
+if(!empty($array))
+$response = array('success' => 1, 'message'=>'Success topics found.','data'=>$array);
 else
 $response = array('success' => 0, 'message'=>'No contacts found.','data'=>"");
 
