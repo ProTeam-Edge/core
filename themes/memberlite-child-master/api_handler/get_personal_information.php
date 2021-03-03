369e10119e13c897b0dc402d194daeb8b9abd6c3
yashpro <eryash@proteamedge.com>
2021-03-03 16:03:56 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_personal_information.php
+++ themes/memberlite-child-master/api_handler/get_personal_information.php
@@ -9,4 +9,9 @@ $input = file_get_contents('php://input');
 $data = json_decode($input);
 $id = $data->id;
 
-echo $id;
+$results = $wpdb->get_results(
+	$wpdb->prepare("SELECT t.*, tt.id AS topic_type_id, tt.form_id, tt.name AS topic_name, tt.icon, tt.topic_type_meta, tt.html_template, t2.image_handle AS profile_handle FROM alpn_topics t LEFT JOIN alpn_topic_types tt ON t.topic_type_id = tt.id LEFT JOIN alpn_topics t2 ON t.connected_id = t2.owner_id AND t2.topic_type_id=5 WHERE t.owner_id = %s && special='user'" , $ $id;)
+ );
+ echo '<pre>';
+ print_r($results);
+ die;
