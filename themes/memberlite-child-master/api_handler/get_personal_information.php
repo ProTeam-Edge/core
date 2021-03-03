14839d1de7dd1fa943cc8f494b06be4fac5f9f00
yashpro <eryash@proteamedge.com>
2021-03-03 16:13:07 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_personal_information.php
+++ themes/memberlite-child-master/api_handler/get_personal_information.php
@@ -8,10 +8,8 @@ global $wpdb;
 $input = file_get_contents('php://input');
 $data = json_decode($input);
 $id = $data->id;
-
-$results = $wpdb->get_results(
-	$wpdb->prepare("SELECT t.*, tt.id AS topic_type_id, tt.form_id, tt.name AS topic_name, tt.icon, tt.topic_type_meta, tt.html_template, t2.image_handle AS profile_handle FROM alpn_topics t LEFT JOIN alpn_topic_types tt ON t.topic_type_id = tt.id LEFT JOIN alpn_topics t2 ON t.connected_id = t2.owner_id AND t2.topic_type_id=5 WHERE t2.owner_id = %s && t2.special='user'" , $id;)
- );
+$sql = "SELECT t.*, tt.id AS topic_type_id, tt.form_id, tt.name AS topic_name, tt.icon, tt.topic_type_meta, tt.html_template, t2.image_handle AS profile_handle FROM alpn_topics t LEFT JOIN alpn_topic_types tt ON t.topic_type_id = tt.id LEFT JOIN alpn_topics t2 ON t.connected_id = t2.owner_id AND t2.topic_type_id=5 WHERE t2.owner_id = '".$id."'&& t2.special='user'";
+$results = $wpdb->get_results($sql);
  echo '<pre>';
  print_r($results);
  die;
