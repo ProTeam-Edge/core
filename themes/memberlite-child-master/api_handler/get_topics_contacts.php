29373bc4b5e4b26baddb71f63bd14a32b66bbb8b
yashpro <eryash@proteamedge.com>
2021-03-03 12:12:22 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_topics_contacts.php
+++ themes/memberlite-child-master/api_handler/get_topics_contacts.php
@@ -12,7 +12,7 @@ $id = $data->id;
 $sql = 'select  topics.about as about, topics.channel_id as channel_id, topics.special as type, users.user_login as name , users.ID as id from alpn_topics as topics inner join wp_users as users on topics.connected_id=users.ID where topics.owner_id = "'.$id.'" and topics.special = "contact" ';
 $result = $wpdb->get_results($sql);
 
-$sql1 = 'select about as about, channel_id as channel_id, special as type from alpn_topics where topics.owner_id = "'.$id.'" and topics.special = "topic" ';
+echo $sql1 = 'select about as about, channel_id as channel_id, special as type from alpn_topics where topics.owner_id = "'.$id.'" and topics.special = "topic" ';
 $result1 = $wpdb->get_results($sql1);
 
 $array = $response= array();
@@ -47,13 +47,15 @@ if(!empty($result) || !empty($result1)) {
 		}
 	}
 	$count = count($array);
+	echo '<pre>';
+	print_r($result1);
 	if(!empty($result1)) {
 		foreach($result1 as $val1) {
 			$count++;
 			$about = 'No about to show here';
 			if(isset( $val1->about) && !empty( $val1->about))
 			{
-				$about = striptags($val->about);
+				$about = striptags($val1->about);
 			}
 			$array['topic'][$count]['name'] = $val1->name;
 			$array['topic'][$count]['channel_id'] = $val1->channel_id;
@@ -62,6 +64,7 @@ if(!empty($result) || !empty($result1)) {
 			
 		}
 	}
+	print_r($array);
 	$response = array('success' => 1, 'message'=>'Contacts found.','data'=>$array);
 } else {
 	$response = array('success' => 0, 'message'=>'No contacts found.','data'=>$array);
