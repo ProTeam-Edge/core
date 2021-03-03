dc61c604c463252b90c975b4bf473aad021c562e
yashpro <eryash@proteamedge.com>
2021-03-03 17:04:29 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_personal_information.php
+++ themes/memberlite-child-master/api_handler/get_personal_information.php
@@ -9,7 +9,7 @@ $input = file_get_contents('php://input');
 $data = json_decode($input);
 $id = $data->id;
 $sql = "SELECT * from alpn_topics where owner_id = ".$id." and special = 'user' and sync_id !=''";
-$results = $wpdb->get_row($sql);
+$results = $wpdb->get_row($sql,ARRAY_A);
 $topic_content = '';
 if(isset($results->topic_content) && !empty($results->topic_content))
 {
