eec2d793da11b17cb6ad1352b83132cb18bdbfe7
yashpro <eryash@proteamedge.com>
2021-03-04 10:25:17 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_topics_contacts.php
+++ themes/memberlite-child-master/api_handler/get_topics_contacts.php
@@ -9,11 +9,14 @@ $input = file_get_contents('php://input');
 $data = json_decode($input);
 $id = $data->id;
 
-$sql = 'select  topics.image_handle as image ,topics.about as about, topics.channel_id as channel_id, topics.special as type, users.user_login as name , users.ID as id from alpn_topics as topics inner join wp_users as users on topics.connected_id=users.ID where topics.owner_id = "'.$id.'" and topics.special = "contact" ';
-$result = $wpdb->get_results($sql);
+$get_user_sql = "SELECT * from alpn_topics where owner_id = ".$id." and special = 'user' and sync_id !=''";
+$get_user_data = $wpdb->get_row($get_user_sql);
 
-$sql1 = 'select image_handle as image ,name, about, channel_id, id from alpn_topics where owner_id = "'.$id.'" and special = "topic" and name!="" ';
-$result1 = $wpdb->get_results($sql1);
+$get_contacts_sql = 'select  topics.image_handle as image ,topics.about as about, topics.channel_id as channel_id, topics.special as type, users.user_login as name , users.ID as id from alpn_topics as topics inner join wp_users as users on topics.connected_id=users.ID where topics.owner_id = "'.$id.'" and topics.special = "contact" ';
+$get_contacts_data = $wpdb->get_results($get_contacts_sql);
+
+$get_topic_sql = 'select image_handle as image ,name, about, channel_id, id from alpn_topics where owner_id = "'.$id.'" and special = "topic" and name!="" ';
+$get_topic_data = $wpdb->get_results($get_topic_sql);
 
 $array = $response= array();
 function striptags($string) {
@@ -41,10 +44,10 @@ if(!empty($result) || !empty($result1)) {
 				$about = striptags($val->about);
 			}
 			if(isset($val->image)) {
-				$final_image = $val->image;
+				$final_image = 'https://storage.googleapis.com/pte_media_store_1/'.$val->image;
 			}
 			$array['contact'][$i]['name'] = $val->name;
-			$array['contact'][$i]['image'] = 'https://storage.googleapis.com/pte_media_store_1/'.$val->image.'';
+			$array['contact'][$i]['image'] = $final_image;
 			$array['contact'][$i]['channel_id'] = $val->channel_id;
 			$array['contact'][$i]['about'] = $about;
 			$array['contact'][$i]['id'] = $val->id;
@@ -62,17 +65,20 @@ if(!empty($result) || !empty($result1)) {
 				$about1 = striptags($val1->about);
 			}
 			if(isset($val1->image)) {
-				$final_image = $val1->image;
+				$final_image = 'https://storage.googleapis.com/pte_media_store_1/'.$val1->image;
 			}
 			$array['topic'][$m]['name'] = $val1->name;
-			$array['topic'][$m]['image'] = 'https://storage.googleapis.com/pte_media_store_1/'.$final_image;
+			$array['topic'][$m]['image'] = $final_image;
 			$array['topic'][$m]['channel_id'] = $val1->channel_id;
 			$array['topic'][$m]['about'] = $about1;
 			$array['topic'][$m]['id'] = $val1->id;
 			$m++;
 		}
 	}
-
+	if(isset($get_user_data->image_handle)) {
+		$final_image = 'https://storage.googleapis.com/pte_media_store_1/'.$get_user_data->image_handle;
+	}
+	$array['user_image'] = $final_image;
 	$response = array('success' => 1, 'message'=>'Contacts found.','data'=>$array);
 } else {
 	$response = array('success' => 0, 'message'=>'No contacts found.','data'=>$array);
