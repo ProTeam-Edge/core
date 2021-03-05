aaf6dc383a779648c39a7360b78717fc1d5aa500
yashpro <eryash@proteamedge.com>
2021-03-05 16:04:06 +0530

    Push Notification Panel
    Push Notification Panel

---
+++ themes/memberlite-child-master/api_handler/submit_device_id.php
@@ -0,0 +1,18 @@
+<?php
+include('/var/www/html/proteamedge/public/wp-blog-header.php');
+include_once('../pte_config.php');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
+$root = $_SERVER['DOCUMENT_ROOT'];
+global $wpdb;
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+$device_token = $data->device_token;
+
+$update_sql= "update wp_users set device_token ='".$device_token."'  where ID =".$id." ";
+$update_data = $wpdb->get_row($update_sql);
+if($update_data)
+	$response = array('success' => 1, 'message'=>'Updated token successfully.','data'=>'1');
+else
+$response = array('success' => 0, 'message'=>'Updation failed. Please try again later','data'=>'0');
+echo json_encode($response); 
