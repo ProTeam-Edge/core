c9e1290f411730f8442513793f62d2b21318881e
yashpro <eryash@proteamedge.com>
2021-03-10 12:22:07 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_single_topic.php
+++ themes/memberlite-child-master/api_handler/get_single_topic.php
@@ -19,47 +19,41 @@ if(isset($results->topic_content) && !empty($results->topic_content))
 	$topic_content_response = json_decode($results->topic_content);
 	if(!empty($topic_content_response))
 	{
-		echo '<pre>';
-		print_r($topic_content_response);
-		die;
-		/* $job_title = $job_email = $job_url = $telephone = $faxnumber = $knowsabout = $carrier =  $description = 'None' ;
-		if(isset($topic_content_response->person_hasoccupation_occupation_occupationalcategory) && !empty($topic_content_response->person_hasoccupation_occupation_occupationalcategory)){
-			$carrier_id = $topic_content_response->person_hasoccupation_occupation_occupationalcategory;
-			$carrier = $businessTypesList[$carrier_id];
-		}
 		
-		$array['person_givenname'] =$topic_content_response->person_givenname;
-		$array['occupation'] =$carrier;
-		$array['person_familyname'] =$topic_content_response->person_familyname;
-		if(isset($topic_content_response->person_jobtitle) && !empty($topic_content_response->person_jobtitle))
-		$job_title = $topic_content_response->person_jobtitle;
-		if(isset($topic_content_response->person_email) && !empty($topic_content_response->person_email))
-		$job_email = $topic_content_response->person_email;
-		if(isset($topic_content_response->person_url) && !empty($topic_content_response->person_url))
-		$job_url = $topic_content_response->person_url;
-		if(isset($topic_content_response->person_telephone) && !empty($topic_content_response->person_telephone))
-		{
-		$telephone = $topic_content_response->person_telephone;
+		 $place_name = $place_streetaddress = $place_addresslocality = $place_addressregion = $place_postalcode = $place_telephone = $place_faxnumber =  $place_url = $place_description = 'None' ;
+		
+		if(isset($topic_content_response->place_name) && !empty($topic_content_response->place_name))
+			$place_name = $topic_content_response->place_name;
+			
+		if(isset($topic_content_response->place_address_postaladdress_streetaddress) && !empty($topic_content_response->place_address_postaladdress_streetaddress)) {
+			$place_streetaddress = $topic_content_response->place_address_postaladdress_streetaddress;
 		}
-		if(isset($topic_content_response->person_faxnumber) && !empty($topic_content_response->person_faxnumber))
-		{
-		$faxnumber = $topic_content_response->person_faxnumber;
+		if(isset($topic_content_response->place_address_postaladdress_addresslocality) && !empty($topic_content_response->place_address_postaladdress_addresslocality)) {
+			$place_addresslocality = $topic_content_response->place_address_postaladdress_addresslocality;
 		}
-		if(isset($topic_content_response->person_knowsabout) && !empty($topic_content_response->person_knowsabout))
-		{
-		$knowsabout = $topic_content_response->person_knowsabout;
+		if(isset($topic_content_response->place_address_postaladdress_addressregion) && !empty($topic_content_response->place_address_postaladdress_addressregion)) {
+			$place_addressregion = $topic_content_response->place_address_postaladdress_addressregion;
 		}
-		if(isset($topic_content_response->person_description) && !empty($topic_content_response->person_description))
-		{
-		$description = $topic_content_response->person_description;
+		if(isset($topic_content_response->place_address_postaladdress_postalcode) && !empty($topic_content_response->place_address_postaladdress_postalcode)) {
+			$place_postalcode = $topic_content_response->place_address_postaladdress_postalcode;
 		}
-		$array['person_jobtitle'] =$job_title;
-		$array['person_email'] =$job_email;
-		$array['person_url'] =$job_url;
-		$array['person_telephone'] =$telephone;
-		$array['person_faxnumber'] =$faxnumber;
-		$array['person_knowsabout'] =$knowsabout;
-		$array['person_description'] =$description; */
+		if(isset($topic_content_response->place_telephone) && !empty($topic_content_response->place_telephone)) 
+			$place_telephone = $topic_content_response->place_telephone;
+		if(isset($topic_content_response->place_faxnumber) && !empty($topic_content_response->place_faxnumber)) 
+			$place_faxnumber = $topic_content_response->place_faxnumber;
+		if(isset($topic_content_response->place_url) && !empty($topic_content_response->place_url)) 
+			$place_url = $topic_content_response->place_url;
+		if(isset($topic_content_response->place_description) && !empty($topic_content_response->place_description)) 
+			$place_description = $topic_content_response->place_description;
+		$array['place_name'] =$place_name;
+		$array['place_streetaddress'] =$place_streetaddress;
+		$array['place_addresslocality'] =$place_addresslocality;
+		$array['place_addressregion'] =$place_addressregion;
+		$array['place_postalcode'] =$place_postalcode;
+		$array['place_telephone'] =$place_telephone;
+		$array['place_faxnumber'] =$place_faxnumber;
+		$array['place_url'] =$place_url;
+		$array['place_description'] =$place_description; 
 	}
 }
 
