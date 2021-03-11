78eb39f815f61f8aae7c7071374c7260bcd3f762
yashpro <eryash@proteamedge.com>
2021-03-11 14:36:38 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/authenticate.php
+++ themes/memberlite-child-master/api_handler/authenticate.php
@@ -23,9 +23,12 @@ if(!empty($email) && !empty($password))
 		
 		if ( $verify && wp_check_password( $password, $verify->data->user_pass, $verify->ID ) ) {
 			$hash = md5('proteamedge'.$verify->data->user_login.$verify->ID.time());
+			$sql = "SELECT * from alpn_topics where owner_id = ".$verify->ID." and special = 'user' and sync_id !=''";
+			$get_alpn_result = $wpdb->get_row($sql);
 			$response_data['ID'] = strval($verify->ID);
 			$response_data['username'] = $verify->data->user_login;
 			$response_data['email'] = $verify->data->user_email;
+			$response_data['alpn_id'] = $get_alpn_result->id;
 			$response_data['token'] = $hash;
 			$update_option = update_option('api_request_token_'.$verify->ID.'',$hash);
 			$update_sql= "update wp_users set device_token ='".$device_token."'  where ID =".$verify->ID." ";
