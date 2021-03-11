46a50c840b43f04148728c79a9d4d42434a11913
yashpro <eryash@proteamedge.com>
2021-03-11 14:56:53 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_topics_contacts.php
+++ themes/memberlite-child-master/api_handler/get_topics_contacts.php
@@ -62,6 +62,7 @@ if(!empty($final_data)) {
 				$array[$val->special][$increment_variable]['channel_id'] = $val->channel_id;
 				$array[$val->special][$increment_variable]['about'] = $about;
 				$array[$val->special][$increment_variable]['id'] = $val->id;
+				$array[$val->special][$increment_variable]['source_type_key'] = $val->source_type_key;
 				if($val->special=='topic'){
 					$t++;
 				}
