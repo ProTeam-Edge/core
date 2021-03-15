e718a7233d16509e97d076dff68a8fba3bb9f9da
yashpro <eryash@proteamedge.com>
2021-03-15 15:23:04 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_tabs.php
+++ themes/memberlite-child-master/api_handler/get_tabs.php
@@ -13,7 +13,18 @@ $id = $data->id;
 
 $businessTypesList = get_custom_post_items('pte_profession', 'ASC');
 $sql = "SELECT t.*, p.access_level, f.pstn_number, tt.id AS topic_type_id, tt.special, tt.type_key, tt.form_id, tt.name AS topic_name, tt.icon, tt.topic_type_meta, tt.html_template, t2.topic_content AS connected_topic_content, t3.name AS owner_name, t3.topic_content AS owner_topic_content, t2.image_handle AS profile_handle FROM alpn_topics t LEFT JOIN alpn_proteams p ON p.topic_id = t.id AND p.owner_id = t.owner_id LEFT JOIN alpn_pstn_numbers f ON f.topic_id = t.id LEFT JOIN alpn_topic_types tt ON t.topic_type_id = tt.id LEFT JOIN alpn_topics t2 ON t2.owner_id = t.connected_id AND t2.special = 'user' LEFT JOIN alpn_topics t3 ON t3.owner_id = t.owner_id AND t3.special = 'user' WHERE t.id = ".$id."";
-$topicData = $wpdb->get_row($sql);
+$results = $wpdb->get_results($sql);
+
+ if (!isset($results[0])) {
+	 $html = "
+	 <div class='pte_topic_error_message'>
+	 		The selected topic has been deleted. Please select another topic or link.
+	 </div>";
+	 echo $html;
+	 exit;
+ }
+
+$topicData = $results[0];
 $topicTypeId = $topicData->topic_type_id;
 $topicSpecial = $topicData->special;
 $topicTypeName = $topicData->topic_name;
@@ -618,10 +629,7 @@ $html .= "
 						";
 
 
-
-echo '<pre>';
-print_r($topicTabs);
-die;
+echo $html;
 if(!empty($array))
 $response = array('success' => 1, 'message'=>'Success data found.','data'=>$array);
 else
