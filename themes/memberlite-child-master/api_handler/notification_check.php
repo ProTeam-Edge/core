e50947913259399138947b0d11903ec4d58fa8b2
yashpro <eryash@proteamedge.com>
2021-03-19 10:16:03 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/notification_check.php
+++ themes/memberlite-child-master/api_handler/notification_check.php
@@ -8,7 +8,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 require_once $root.'/wp-content/themes/memberlite-child-master/api_handler/sdk/vendor/autoload.php';
 use Twilio\Jwt\AccessToken;
 use Twilio\Jwt\Grants\ChatGrant;
-use Twilio\Jwt\Client;
+use Twilio\Rest\Client;
 
 
 $twilioAccountSid = ACCOUNT_SID;
