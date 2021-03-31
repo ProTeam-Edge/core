a12fdb000c0861b2e6587217e94d5f59d0b10aa1
yashpro <eryash@proteamedge.com>
2021-03-31 11:46:09 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_refreshed_token.php
+++ themes/memberlite-child-master/api_handler/get_refreshed_token.php
@@ -6,13 +6,14 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 global $wpdb;
 $input = file_get_contents('php://input');
 $data = json_decode($input);
-$sql = 'select u.device_token from alpn_topics as a JOIN alpn_topics as b on a.connected_topic_id = b.id JOIN wp_users as u on b.owner_id = u.ID where a.id='.$val->id.'';
-$data = $wpdb->get_row($sql);
-if($data){
-	$dId = $data->device_token;
+$source_key = $data->source_key;
+$channelId = $data->channelId;
+$id = $data->id;
+if($source_key=='core_contact') {
+	$sql = 'select u.device_token from alpn_topics as a JOIN alpn_topics as b on a.connected_topic_id = b.id JOIN wp_users as u on b.owner_id = u.ID where a.id='.$id.'';
+	$data = $wpdb->get_row($sql);
+	$token = $data->device_token;
 }
-else {
-	$dId = '';
-}
-$response = array('success' => 0, 'message'=>'No token found.','data'=>$dId,);
+
+$response = array('success' => 1, 'message'=>'No token found.','data'=>$token,);
 echo json_encode($response); 
