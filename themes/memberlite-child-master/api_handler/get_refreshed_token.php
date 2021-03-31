4c33f95d755b80ad6915c72a3586ea269f9f9080
yashpro <eryash@proteamedge.com>
2021-03-31 13:53:32 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_refreshed_token.php
+++ themes/memberlite-child-master/api_handler/get_refreshed_token.php
@@ -11,14 +11,14 @@ $source_key = $data->source_key;
 $channelId = $data->channelId;
 $id = $data->id;
 $identity = $data->identity;
-$token = [];
+$token = array();
 if(!empty($source_key) && !empty($channelId) && !empty($id))
 {
 	if($source_key=='core_contact') {
 		$sql = 'select u.device_token from alpn_topics as a JOIN alpn_topics as b on a.connected_topic_id = b.id JOIN wp_users as u on b.owner_id = u.ID where a.id='.$id.'';
 
 		$result = $wpdb->get_row($sql);
-		$token[] = $result->device_token;
+		$token[0] = $result->device_token;
 	}
 	else {
 		echo $sql = 'select u.device_token , u.ID from alpn_proteams as ap join alpn_topics as ato on ap.proteam_member_id=ato.id join wp_users as u on ato.connected_id=u.ID where ap.topic_id="'.$id.'"';
