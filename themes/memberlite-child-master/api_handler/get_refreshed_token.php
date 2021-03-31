670b0874a79939c636bbbe339722f63dcd8d3985
yashpro <eryash@proteamedge.com>
2021-03-31 11:04:41 +0530

    api application
    api application

---
+++ themes/memberlite-child-master/api_handler/get_refreshed_token.php
@@ -0,0 +1,18 @@
+<?php
+include_once('../pte_config.php');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); 
+$root = $_SERVER['DOCUMENT_ROOT'];
+global $wpdb;
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+$sql = 'select u.device_token from alpn_topics as a JOIN alpn_topics as b on a.connected_topic_id = b.id JOIN wp_users as u on b.owner_id = u.ID where a.id='.$val->id.'';
+$data = $wpdb->get_row($sql);
+if($data){
+	$dId = $data->device_token;
+}
+else {
+	$dId = '';
+}
+$response = array('success' => 0, 'message'=>'No token found.','data'=>$dId,);
+echo json_encode($response); 
