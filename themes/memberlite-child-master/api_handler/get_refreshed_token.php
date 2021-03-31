857cc4f2c5994609a1b544fca794e1ea8ae42e98
yashpro <eryash@proteamedge.com>
2021-03-31 12:18:17 +0530

    api application
    api application

--- themes/memberlite-child-master/api_handler/get_refreshed_token.php
+++ themes/memberlite-child-master/api_handler/get_refreshed_token.php
@@ -11,9 +11,8 @@ $channelId = $data->channelId;
 $id = $data->id;
 $token = '';
 if($source_key=='core_contact') {
-		$sql = 'select u.device_token from alpn_topics as a JOIN alpn_topics as b on a.connected_topic_id = b.id JOIN wp_users as u on b.owner_id = u.ID where a.id="'.$id.'"';
-	
-	$result = $wpdb->get_row($sql);
-	echo '<pre>';
-	print_r($result);
+$sql = 'select u.device_token from alpn_topics as a JOIN alpn_topics as b on a.connected_topic_id = b.id JOIN wp_users as u on b.owner_id = u.ID where a.id="'.$id.'"';
+$result = $wpdb->get_row($sql);
+echo '<pre>';
+print_r($result);
 }
