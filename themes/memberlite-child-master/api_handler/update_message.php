22cd9baf1cc56ad42e8752e2f91c8a9168c3722c
yashpro <eryash@proteamedge.com>
2021-04-14 15:51:45 +0530

    api update
    api update

--- themes/memberlite-child-master/api_handler/update_message.php
+++ themes/memberlite-child-master/api_handler/update_message.php
@@ -22,7 +22,7 @@ if(!empty($data)) {
 		$message = $twilio->chat->v2->services($serviceSid)
                             ->channels($channel_id)
                             ->messages($message_id)
-                            ->update(["body" => "body"]);
+                            ->update(["body" =>$message]);
 		
 		$response = array('success' => 1, 'message'=>'Message updated successfully.','data'=>'');
 	} catch (Exception $e) {
