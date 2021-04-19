300a06bc1d70bdf8003f715d71b9e02b03e4b18c
yashpro <eryash@proteamedge.com>
2021-04-19 13:34:00 +0530

    api update
    api update

--- themes/memberlite-child-master/alpn_client.js
+++ themes/memberlite-child-master/alpn_client.js
@@ -2683,23 +2683,18 @@ function pte_setup_window_onload() {
 	}
 
 }
-jQuery(window).load(function() {
-/* 	pte_external =  pte_chrome_extension || pte_topic_manager_loaded || pte_template_editor_loaded;
-console.log("WORKED ONLOAD SETUP");
-			pte_setup_window_onload(); */
-});
-jQuery( document ).ready( function(){
+
+jQuery( window ).load( function(){
 
 
 	console.log("DOC READY");
 
 	pte_external =  pte_chrome_extension || pte_topic_manager_loaded || pte_template_editor_loaded;
 
-	/* window.onload = function() {
-		
+
 			console.log("WORKED ONLOAD SETUP");
 			pte_setup_window_onload();
-	} */
+
 
 	if (!pte_external) {pte_get_active_video_rooms();}
 
