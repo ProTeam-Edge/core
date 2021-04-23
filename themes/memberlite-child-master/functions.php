d3231c602938af630e18a1198afa7a12ec1be6c0
yashpro <eryash@proteamedge.com>
2021-04-23 10:48:22 +0530

    api update
    api update

--- themes/memberlite-child-master/functions.php
+++ themes/memberlite-child-master/functions.php
@@ -14,15 +14,12 @@
 
     // run code
 } */
-die;
 include_once('pte_config.php');
 $domainName = PTE_HOST_DOMAIN_NAME;
 // verify added nonce before submission for wpforms
 use PascalDeVink\ShortUuid\ShortUuid;
-function disable_tml_ajax( $form_name, $form ) {
-    $form->remove_attribute( 'data-ajax' );
-}
-add_action( 'tml_registered_form', 'disable_tml_ajax', 10, 2 );
+echo 'test';
+
 function wpf_dev_process_before( $entry, $form_data ) {
 
 	$passed = 0;
