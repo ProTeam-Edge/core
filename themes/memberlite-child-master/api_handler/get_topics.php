1cb48baf4eb3fba23e8d7794c3030e71bccd7048
yashpro <eryash@proteamedge.com>
2021-04-26 16:15:11 +0530

    api update
    api update

--- themes/memberlite-child-master/api_handler/get_topics.php
+++ themes/memberlite-child-master/api_handler/get_topics.php
@@ -164,10 +164,8 @@ foreach ($fullMap as $key => $value) {
 					if ($topicLogoHandle) {
 						$topicLogoUrl = "{$ppCdnBase}{$topicLogoHandle}";
 					}
-					echo $topicLogoUrl;
-					die;
 					$friendlyLogoName = $value['friendly'];
-					$replaceStrings[$friendlyLogoName] = '';
+					$replaceStrings[$friendlyLogoName] = $topicLogoUrl;
 					if ($hidden) {$showLogoAccordion = 'none';}
 				break;
 			}
