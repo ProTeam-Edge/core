6f32908ff1040ba9dd7fdacf571dbd574446d644
yashpro <eryash@proteamedge.com>
2021-04-26 13:17:55 +0530

    api update
    api update

--- themes/memberlite-child-master/api_handler/get_topics.php
+++ themes/memberlite-child-master/api_handler/get_topics.php
@@ -12,7 +12,23 @@ $id = $data->id;
 $userID = $data->userID;
 $type =  $data->type;
 
+function convert_from_latin1_to_utf8_recursively($dat)
+   {
+      if (is_string($dat)) {
+         return utf8_encode($dat);
+      } elseif (is_array($dat)) {
+         $ret = [];
+         foreach ($dat as $i => $d) $ret[ $i ] = self::convert_from_latin1_to_utf8_recursively($d);
 
+         return $ret;
+      } elseif (is_object($dat)) {
+         foreach ($dat as $i => $d) $dat->$i = self::convert_from_latin1_to_utf8_recursively($d);
+
+         return $dat;
+      } else {
+         return $dat;
+      }
+   }
 $businessTypesList = get_custom_post_items('pte_profession', 'ASC');
 $sql = "SELECT t.*, p.access_level, f.pstn_number, tt.id AS topic_type_id, tt.special, tt.type_key, tt.form_id, tt.name AS topic_name, tt.icon, tt.topic_type_meta, tt.html_template, t2.topic_content AS connected_topic_content, t3.name AS owner_name, t3.topic_content AS owner_topic_content, t2.image_handle AS profile_handle FROM alpn_topics t LEFT JOIN alpn_proteams p ON p.topic_id = t.id AND p.owner_id = t.owner_id LEFT JOIN alpn_pstn_numbers f ON f.topic_id = t.id LEFT JOIN alpn_topic_types tt ON t.topic_type_id = tt.id LEFT JOIN alpn_topics t2 ON t2.owner_id = t.connected_id AND t2.special = 'user' LEFT JOIN alpn_topics t3 ON t3.owner_id = t.owner_id AND t3.special = 'user' WHERE t.id = ".$id."";
 $results = $wpdb->get_results($sql);
@@ -362,9 +378,10 @@ foreach($topicTabs as $keys=>$vals) {
 else {
 	$topicTabs = $replaceStrings;
 }
+$fix_array = convert_from_latin1_to_utf8_recursively($topicTabs);
 if(!empty($topicTabs))
 {
-	$response = array('success' => 1, 'message'=>'Success data found.','data'=>$topicTabs);
+	$response = array('success' => 1, 'message'=>'Success data found.','data'=>$fix_array);
 }
 else
 $response = array('success' => 0, 'message'=>'No data found.','data'=>null);
