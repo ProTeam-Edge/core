70f15aa88ad15d9d40ec1b6873a606a3496119f5
yashpro <eryash@proteamedge.com>
2021-04-26 13:08:04 +0530

    api update
    api update

--- themes/memberlite-child-master/api_handler/get_topics.php
+++ themes/memberlite-child-master/api_handler/get_topics.php
@@ -368,6 +368,9 @@ if(!empty($topicTabs))
 }
 else
 $response = array('success' => 0, 'message'=>'No data found.','data'=>null);
-
+$encode = json_encode($response); 
+echo '<pre>';
+print_r($encode);
+die;
 echo json_encode($response); 
 die;
