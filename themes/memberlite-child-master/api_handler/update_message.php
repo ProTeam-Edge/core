4314d90632df5188b95ec7cd3b2128fcb34632e9
yashpro <eryash@proteamedge.com>
2021-04-27 12:00:03 +0530

    api update
    api update

--- themes/memberlite-child-master/api_handler/update_message.php
+++ themes/memberlite-child-master/api_handler/update_message.php
@@ -17,8 +17,12 @@ $twilio = new Client($sid, $token);
 $message_id = $data->message_id;
 $channel_id = $data->channel_id;
 $message = $data->message;
-if(!empty($data)) {
-	try {
+$apiToken =  $data->apiToken;
+$userID = $data->userID;
+if(!empty($message_id) && !empty($channel_id) && !empty($message) && !empty($apiToken) && !empty($userID)) {
+	$get_token = get_option('api_request_token_'.$userID.'');
+	if($get_token==$apiToken) {
+		try {
 		$message = $twilio->chat->v2->services($serviceSid)
                             ->channels($channel_id)
                             ->messages($message_id)
@@ -28,6 +32,10 @@ if(!empty($data)) {
 	} catch (Exception $e) {
 		$response = array('success' => 2, 'message'=>'There was some error','data'=>$e->getMessage());
 	}
+	} else {
+		$response = array('success' => 2, 'message'=>'Not a valid token','data'=>null);
+	}
+	
 } else {
 	$response = array('success' => 0, 'message'=>'No required parameters found.','data'=>'');
 }
