bf9e6eb806f26f1e981e5ad84586bf001f087c87
yashpro <eryash@proteamedge.com>
2021-04-27 13:45:48 +0530

    api update
    api update

--- themes/memberlite-child-master/api_handler/delete_message.php
+++ themes/memberlite-child-master/api_handler/delete_message.php
@@ -11,9 +11,9 @@ use Twilio\Rest\Client;
 $array = array();
 $input = file_get_contents('php://input');
 $data = json_decode($input);
-if(!empty($message_id) && !empty($channel_id)  && !empty($apiToken) && !empty($userID)) {
+if(!empty($data->message_id) && !empty($data->channel_id)  && !empty($data->apiToken) && !empty($data->userID)) {
 $get_token = get_option('api_request_token_'.$userID.'');
-	if($get_token==$apiToken) {
+if($get_token==$apiToken) {
 $sid    = ACCOUNT_SID;
 $token  =AUTHTOKEN;
 $serviceSid = CHATSERVICESID;
