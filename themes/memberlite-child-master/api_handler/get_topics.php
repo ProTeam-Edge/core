e1da68e90b04e73512e20c75d24a404c1349fe55
yashpro <eryash@proteamedge.com>
2021-04-27 12:25:18 +0530

    api update
    api update

--- themes/memberlite-child-master/api_handler/get_topics.php
+++ themes/memberlite-child-master/api_handler/get_topics.php
@@ -11,12 +11,8 @@ $data = json_decode($input);
 $id = $data->id;
 $userID = $data->userID;
 $type =  $data->type;
-$apiToken =  $data->apiToken;
 
-if(!empty($id) && !empty($userID) && !empty($type) && !empty($apiToken)) {
-$get_token = get_option('api_request_token_'.$userID.'');
-if($get_token==$apiToken)
-{
+
 $businessTypesList = get_custom_post_items('pte_profession', 'ASC');
 $sql = "SELECT t.*, p.access_level, f.pstn_number, tt.id AS topic_type_id, tt.special, tt.type_key, tt.form_id, tt.name AS topic_name, tt.icon, tt.topic_type_meta, tt.html_template, t2.topic_content AS connected_topic_content, t3.name AS owner_name, t3.topic_content AS owner_topic_content, t2.image_handle AS profile_handle FROM alpn_topics t LEFT JOIN alpn_proteams p ON p.topic_id = t.id AND p.owner_id = t.owner_id LEFT JOIN alpn_pstn_numbers f ON f.topic_id = t.id LEFT JOIN alpn_topic_types tt ON t.topic_type_id = tt.id LEFT JOIN alpn_topics t2 ON t2.owner_id = t.connected_id AND t2.special = 'user' LEFT JOIN alpn_topics t3 ON t3.owner_id = t.owner_id AND t3.special = 'user' WHERE t.id = ".$id."";
 $results = $wpdb->get_results($sql);
@@ -372,11 +368,6 @@ if(!empty($topicTabs))
 	$response = array('success' => 1, 'message'=>'Success data found.','data'=>$topicTabs);
 }
 else
-$response = array('success' => 0, 'message'=>'No data found.','data'=>null);
-}  else {
-	$response = array('success' => 2, 'message'=>'Not a valid token','data'=>null);
-}
-}else {
-	$response = array('success' => 2, 'message'=>'No required parameters provided.','data'=>null);
-}
+$response = array('success' => 0, 'message'=>'No data found.','data'=>"");
+
 echo json_encode($response); 
--- themes/memberlite-child-master/api_handler/update_message.php
+++ themes/memberlite-child-master/api_handler/update_message.php
@@ -17,12 +17,8 @@ $twilio = new Client($sid, $token);
 $message_id = $data->message_id;
 $channel_id = $data->channel_id;
 $message = $data->message;
-$apiToken =  $data->apiToken;
-$userID = $data->userID;
-if(!empty($message_id) && !empty($channel_id) && !empty($message) && !empty($apiToken) && !empty($userID)) {
-	$get_token = get_option('api_request_token_'.$userID.'');
-	if($get_token==$apiToken) {
-		try {
+if(!empty($data)) {
+	try {
 		$message = $twilio->chat->v2->services($serviceSid)
                             ->channels($channel_id)
                             ->messages($message_id)
@@ -32,10 +28,6 @@ if(!empty($message_id) && !empty($channel_id) && !empty($message) && !empty($api
 	} catch (Exception $e) {
 		$response = array('success' => 2, 'message'=>'There was some error','data'=>$e->getMessage());
 	}
-	} else {
-		$response = array('success' => 2, 'message'=>'Not a valid token','data'=>null);
-	}
-	
 } else {
 	$response = array('success' => 0, 'message'=>'No required parameters found.','data'=>'');
 }
