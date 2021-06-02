09c1de6f89ed870dc1ba358c8644ba3f379738b9
anubhav91 <eryash@proteamedge.com>
2021-06-02 15:00:34 +0530

    firebase integration for web
    firebase integration for web

--- themes/memberlite-child-master/alpn_client.js
+++ themes/memberlite-child-master/alpn_client.js
@@ -2606,7 +2606,10 @@ function pte_setup_window_onload() {
 									alert(payload)
 									alert('got notification')
 								
-								});
+								}).catch((err) => {
+									console.log(err);
+									alert('error')
+										});
 							} else {
 								// no Firebase library imported or Firebase library wasn't correctly initialized
 							}
