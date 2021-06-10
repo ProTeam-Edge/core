b5662f7ffd58bf3e7f978ada215ab28106326bf4
anubhav91 <eryash@proteamedge.com>
2021-06-10 15:43:37 +0530

    firebase integration for web
    firebase integration for web

--- themes/memberlite-child-master/api_handler/notification_check.php
+++ themes/memberlite-child-master/api_handler/notification_check.php
@@ -41,7 +41,7 @@ try {
     ->notifications
     ->create([
                  "body" => "Hello this is test",
-                 "identity" => ["fcmindentity"]
+                 "identity" => ["128"]
              ]
     );
     echo '<pre>';
