b89ff636fa7b9b76331ecf26cd4476558bc4e0ae
anubhav91 <eryash@proteamedge.com>
2021-06-11 11:04:14 +0530

    firebase integration for web
    firebase integration for web

--- themes/memberlite-child-master/api_handler/saveFcm.php
+++ themes/memberlite-child-master/api_handler/saveFcm.php
@@ -4,7 +4,7 @@ global $wpdb;
 if(isset($_POST['token']) && !empty($_POST['token'])) {
     $token = $_POST['token'];
     $userId = $_POST['userId'];
-    $sql = 'update '.$wpdb->prefix.'users set device_token_web = '.$token.' where ID = '.$userId.'';
+    $sql = 'update '.$wpdb->prefix.'users set device_token_web = "'.$token.'" where ID = "'.$userId.'"';
     $data = $wpdb->query($sql);
     if($data)
     $output = 'success'; 
