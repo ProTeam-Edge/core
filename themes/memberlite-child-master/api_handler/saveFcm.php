8b068a60b2a916f5bc190c69e77301ae86fffb45
anubhav91 <eryash@proteamedge.com>
2021-06-11 10:56:16 +0530

    firebase integration for web
    firebase integration for web

--- themes/memberlite-child-master/api_handler/saveFcm.php
+++ themes/memberlite-child-master/api_handler/saveFcm.php
@@ -1,6 +1,15 @@
 <?php
 include('/var/www/html/proteamedge/public/wp-blog-header.php');
-echo '<pre>';
-print_r($_POST);
-DIE;
+if(isset($_POST['token']) && !empty($_POST['token'])) {
+    $token = $_POST['token'];
+    $userId = $_POST['userId'];
+    $sql = 'update '.$wpdb->prefix.'users set device_token_web = '.$token.' where ID = '.$userId.'';
+    $data = $wpdb->query($sql);
+    if($data)
+    $output = 'success'; 
+}
+else 
+    $output = 'Not allwed';
+
+echo json_encode($output);
 ?>
